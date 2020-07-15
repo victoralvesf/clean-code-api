c2cdefb017840da0cc972f263cbe412a7e5d9803
Victor Alves <victoralves@fortics.com.br>
2020-07-14 23:41:10 -0300

    feat: ensure SignUpController returns 400 if the password confirmation is different

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -15,7 +15,11 @@ export class SignUpController implements Controller {
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) return badRequest(new MissingParamError(field))
       }
-      const isValid = this.emailValidator.isValid(httpRequest.body.email)
+      const { email, password, passwordConfirmation } = httpRequest.body
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
+      const isValid = this.emailValidator.isValid(email)
       if (!isValid) return badRequest(new InvalidParamError('email'))
     } catch (error) {
       return serverError()
